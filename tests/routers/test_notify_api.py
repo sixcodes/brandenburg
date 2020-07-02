2647b7797c35e8db96f1119d14d9ace2e6c07290
Jesue Junior <jesuesousa@gmail.com>
2020-07-02 00:42:05 -0300

    Close #14 implemented basic notify endpoint

--- tests/routers/test_notify_api.py
+++ tests/routers/test_notify_api.py
@@ -22,3 +22,15 @@ def test_api_good_request(client):
     res = client.post("/v1/notify/", json=NOTIFY, headers=HEADERS)
     assert res.status_code == 202
     assert res.json() == {"status": "OK", "message": "Notification Accepted!"}
+
+
+def test_api_wrong_request(client):
+    res = client.post("/v1/notify/", json={"by": "sms"}, headers=HEADERS)
+    assert res.status_code == 422
+    assert res.json() == {
+        'detail': [
+            {'loc': ['body', 'contact'], 'msg': 'field required', 'type': 'value_error.missing'},
+            {'loc': ['body', 'data'], 'msg': 'field required', 'type': 'value_error.missing'},
+            {'loc': ['body', 'template_name'], 'msg': 'field required', 'type': 'value_error.missing'},
+        ]
+    }
