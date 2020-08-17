9ccfdf19e5e43da39a4536b9be388053baae0870
Jesue Junior <jesuesousa@gmail.com>
2020-08-17 20:47:00 -0300

    Organizing packages P1

---
+++ brandenburg/brokers/email.py
@@ -0,0 +1,9 @@
+from brandenburg.toolbox.logger import log
+from config import settings
+from interfaces import BrokerInterface
+
+LOGGER = log.get_logger(__name__)
+
+
+class EmailBroker(BrokerInterface):
+    pass
