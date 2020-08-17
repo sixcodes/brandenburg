229852261f5fd9af3f5e3b051b3f165529eb5bb8
Jesue Junior <jesuesousa@gmail.com>
2020-08-17 20:47:31 -0300

    Organizing packages P1

--- brandenburg/providers/aws.py
+++ brandenburg/providers/aws.py
@@ -10,7 +10,11 @@ class AWS(ProviderInterface):
     """
 
     def get_credentials(self):
-        raise NotImplementedError("get_credentials method is not implemented!")
+        return boto3.Session(
+            region_name="us-east-1",
+            aws_access_key_id=settings.AWS_SERVER_PUBLIC_KEY,
+            aws_secret_access_key=settings.AWS_SERVER_SECRET_KEY,
+        )
 
     def publish(self, topic: str, data, **kwargs):
         raise NotImplementedError("publish method is not implemented!")
@@ -20,3 +24,41 @@ class AWS(ProviderInterface):
 
     def upload_file(self):
         pass
+
+    @classmethod
+    def send_sms(cls, phone: str, message: str, type: str = "Transactional") -> bool:
+        # FIXME: Turn it generic to just send sms
+
+        sns = cls.get_session.client("sns")
+
+        try:
+            response = sns.publish(
+                PhoneNumber=phone,
+                Message=message,
+                MessageAttributes={
+                    "AWS.SNS.SMS.SenderID": {"DataType": "String", "StringValue": "BRANDENBURG"},
+                    "AWS.SNS.SMS.SMSType": {"DataType": "String", "StringValue": type.title()},
+                },
+            )
+            logger.info(response)
+            if response.get("ResponseMetadata", {}).get("HTTPStatusCode") == 200:
+
+                return True
+        except InvalidParameterException as ex:
+            logger.info(ex)  # FIXME: temp dumb solution
+            return False
+
+    @classmethod
+    def send_mail(cls, subject: str, sender: str, to_address: str, body: str) -> bool:
+        ses = cls.get_session.client("ses")
+        response = ses.send_email(
+            Destination={"ToAddresses": [to_address]},
+            Message={
+                "Body": {"Html": {"Charset": "UTF-8", "Data": body}},
+                "Subject": {"Charset": "UTF-8", "Data": subject},
+            },
+            Source=sender,
+        )
+
+        if response.get("ResponseMetadata", {}).get("HTTPStatusCode") == 200:
+            return True
