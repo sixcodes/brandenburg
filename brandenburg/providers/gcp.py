bec76e222e4068e97682ddf7b61a3a3a390cbba5
Jesue Junior <jesuesousa@gmail.com>
2020-08-24 19:22:55 -0300

    Just start pubSUB connection once

--- brandenburg/providers/gcp.py
+++ brandenburg/providers/gcp.py
@@ -14,13 +14,57 @@ from brandenburg.toolbox.logger import log
 logger = log.get_logger(__name__)
 
 
+def pub_client():
+    # Configure the retry settings. Defaults will be overwritten.
+    google_credentials: Json = settings.GOOGLE_CREDENTIALS
+    if google_credentials:
+        credentials = Credentials.from_service_account_info(google_credentials)
+    else:
+        credentials = Credentials()
+
+    retry_settings = {
+        "interfaces": {
+            "google.pubsub.v1.Publisher": {
+                "retry_codes": {
+                    "publish": [
+                        "ABORTED",
+                        "CANCELLED",
+                        "DEADLINE_EXCEEDED",
+                        "INTERNAL",
+                        "RESOURCE_EXHAUSTED",
+                        "UNAVAILABLE",
+                        "UNKNOWN",
+                    ]
+                },
+                "retry_params": {
+                    "messaging": {
+                        "initial_retry_delay_millis": 100,  # default: 100
+                        "retry_delay_multiplier": 1.3,  # default: 1.3
+                        "max_retry_delay_millis": 60000,  # default: 60000
+                        "initial_rpc_timeout_millis": 5000,  # default: 25000
+                        "rpc_timeout_multiplier": 1.0,  # default: 1.0
+                        "max_rpc_timeout_millis": 600000,  # default: 30000
+                        "total_timeout_millis": 600000,  # default: 600000
+                    }
+                },
+                "methods": {"Publish": {"retry_codes_name": "publish", "retry_params_name": "messaging"}},
+            }
+        }
+    }
+    logger.debug("<<<<<<<<<< create a connection >>>>>>>>>")
+    return pubsub_v1.PublisherClient(client_config=retry_settings, credentials=credentials)
+
+
+publisher_client = pub_client()
+
+
 class GCP(ProviderInterface):
     @lru_cache(512)
     def get_credentials(self):
         """
         TODO: checkif credentials exist if not try to auth with local credentials
         """
-        scopes: Tuple[str] = (
+        scopes: Tuple[str, str, str] = (
             "https://www.googleapis.com/auth/bigquery",
             "https://www.googleapis.com/auth/cloud-platform",
             "https://www.googleapis.com/auth/drive",
@@ -55,41 +99,9 @@ class GCP(ProviderInterface):
             TODO: handle exceptions and retries
             Future: https://github.com/googleapis/python-api-core/blob/02d25799243bd76ac76423a08c063a2bee8d11e4/google/api_core/future/base.py#L23
         """
-        # Configure the retry settings. Defaults will be overwritten.
-        retry_settings = {
-            "interfaces": {
-                "google.pubsub.v1.Publisher": {
-                    "retry_codes": {
-                        "publish": [
-                            "ABORTED",
-                            "CANCELLED",
-                            "DEADLINE_EXCEEDED",
-                            "INTERNAL",
-                            "RESOURCE_EXHAUSTED",
-                            "UNAVAILABLE",
-                            "UNKNOWN",
-                        ]
-                    },
-                    "retry_params": {
-                        "messaging": {
-                            "initial_retry_delay_millis": 100,  # default: 100
-                            "retry_delay_multiplier": 1.3,  # default: 1.3
-                            "max_retry_delay_millis": 60000,  # default: 60000
-                            "initial_rpc_timeout_millis": 5000,  # default: 25000
-                            "rpc_timeout_multiplier": 1.0,  # default: 1.0
-                            "max_rpc_timeout_millis": 600000,  # default: 30000
-                            "total_timeout_millis": 600000,  # default: 600000
-                        }
-                    },
-                    "methods": {"Publish": {"retry_codes_name": "publish", "retry_params_name": "messaging"}},
-                }
-            }
-        }
 
         topic_name: str = f"projects/{settings.GOOGLE_PROJECT_ID}/topics/{topic}"
-        # TODO: This is slow down the connection, must be a singletron
-        client = pubsub_v1.PublisherClient(client_config=retry_settings, credentials=self.get_credentials())
-        future = client.publish(topic_name, data, **attrs)
+        future = publisher_client.publish(topic_name, data, **attrs)
         logger.info(f"Futures: {future.result()}")
 
     def upload_file(self, path: str, file: bytes, **kwargs):
